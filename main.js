19b6553e2adb2dd5bf8abbf84b57428202c2140b
abihaladha456 <119162098+abihaladha456@users.noreply.github.com>
2023-07-02 11:31:42 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+
+function displayWelcomeMessage() {
+    var welcomeMessage = "Welcome to Tiny Gymnasts!";
+    console.log(welcomeMessage);
+  }
+  
+
+  function calculateSquare(number) {
+    return number * number;
+  }
+  
+
+  displayWelcomeMessage();
+  
+  var number = 5;
+  var square = calculateSquare(number);
+  console.log("The square of " + number + " is: " + square);
+  
+
+function Trifit() {
+  var link = "https://trifit.com.pk/";
+
+  var newTab = window.open(link, "_blank");
+  newTab.focus();
+}
+
+var button = document.getElementById("show-info-button"); 
+
+button.addEventListener("click", whenButtonClicked);
+
+
+function Platinum() {
+  var link = "https://platinumfitnesspk.com/";
+
+  var newTab = window.open(link, "_blank");
+  newTab.focus();
+}
+
+var button = document.getElementById("show-button"); 
+
+button.addEventListener("click", whenButtonClicked);
+
+function GymShim() {
+  var link ="https://www.gymshim.com/";
+
+  var newTab = window.open(link, "_blank");
+  newTab.focus();
+}
+
+var button = document.getElementById("button"); 
+
+button.addEventListener("click", whenButtonClicked);
+
+
+function Velocity() {
+  var link ="https://velocityx.com/";
+
+  var newTab = window.open(link, "_blank");
+  newTab.focus();
+}
+
+var button = document.getElementById("button5"); 
+
+button.addEventListener("click", whenButtonClicked);
+
+function Evolution() {
+  var link = "https://bodyevolution.pk/";
+
+  var newTab = window.open(link, "_blank");
+  newTab.focus();
+}
+
+var button = document.getElementById("show-info1"); 
+
+button.addEventListener("click", whenButtonClicked);
